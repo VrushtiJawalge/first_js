14ed0982948a594b1d2982c53b38e3e2d7ce68b6
VrushtiJawalge <jawalgevrushti@gmail.com>
2025-07-24 10:12:54 +0530

    Add files via upload

---
+++ ifelse.js
@@ -0,0 +1,10 @@
+  let num=prompt("enter a number");
+        if(num>0){
+            console.log("number is positive");
+        }
+         if(num0){
+            console.log("number is positive");
+        }
+        else{
+             console.log("number is negative");
+        }
